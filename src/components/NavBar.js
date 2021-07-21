3e56a15c42313a4e907bcb77bf132b98d44897c2
Rosa <rosarenee@att.net>
2021-07-20 22:00:44 -0500

    done and fixed the home and about tabs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,8 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+    <a href="#home">About</a>
     </nav>
   );
 }
